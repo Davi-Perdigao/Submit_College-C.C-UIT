2e271c95aa5c21d14df7fa3c3e5f9a353ef4ab93
Davi Perdigão <davivcperdigao@gmail.com>
2021-08-25 19:26:48 -0300

    04

--- 4º Período/POO I/Exercícios Iniciais/Pessoa/Main.java
+++ 4º Período/POO I/Exercícios Iniciais/Pessoa/Main.java
@@ -1,5 +1,20 @@
-package Exercícios Iniciais.Pessoa;
+import java.util.Scanner;
 
-public class Main {
-    
+public class main {
+        public static void main(String args[]){
+        Scanner input = new Scanner(System.in); 
+        Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+
+        p1.setNome(input.nextLine());
+        p1.setEndereco(input.nextLine());
+        p1.setTelefone(input.nextLine());
+
+        p2.setNome(input.nextLine());
+        p2.setEndereco(input.nextLine());
+        p2.setTelefone(input.nextLine());
+
+        System.out.println(pessoa1.imprimir());
+        System.out.println(pessoa2.imprimir());
+    }
 }
--- 4º Período/POO I/Exercícios Iniciais/Pessoa/Pessoa.java
+++ 4º Período/POO I/Exercícios Iniciais/Pessoa/Pessoa.java
@@ -1,5 +1,40 @@
-package Exercícios Iniciais.Pessoa;
+/*
+- Escreva um código em Java que apresente a classe Pessoa, com atributos nome, 
+endereço e telefone e, o método imprimir (Imprime todos os dados da pessoa). O 
+método imprimir deve mostrar na tela os valores de todos os atributos. Não esqueça de 
+adicionar os métodos Get,s e Set's. Crie a classe TestaPessoa, instancie 2 pessoas e teste 
+os métodos. Veja que cada pessoa possui seus próprios atributos.
+*/
 
 public class Pessoa {
     
+    public String nome;
+    public String endereco;
+    public String telefone;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+ 
+    public String getNome(){
+        return this.nome;
+    }
+    public String getEndereco(){
+        return this.endereco;
+    }
+    public String getTelefone(){
+        return this.telefone;
+    }
+
+    public String imprimir(){
+        return ("\nNome: "+getNome()+
+        "\nEndereco: "+getEndereco()+
+        "\nTelefone: "+getTelefone());
+    }
 }
