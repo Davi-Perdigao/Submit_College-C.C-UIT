812e2e3b0873b22809b55e0cc0cb961ca49b2448
Davi Perdigão <davivcperdigao@gmail.com>
2021-08-27 22:22:41 -0300

    IMC

--- 4º Período/POO I/Exercício 3 (pessoa 2.0)/Main.java
+++ 4º Período/POO I/Exercício 3 (pessoa 2.0)/Main.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String args[]){
+
+        Scanner entrada = new Scanner (System.in);
+        Pessoa joao = new Pessoa();
+        Pessoa maria = new Pessoa();
+
+        System.out.println("Nome: ");
+        System.out.println("1ª pessoa: ");
+        joao.setNome(entrada.nextLine());
+        System.out.println("2ª pessoa: ");
+        maria.setNome(entrada.nextLine());
+
+        System.out.println("Sexo: ");
+        System.out.println("1ª pessoa: ");
+        joao.setsexo(entrada.nextLine());
+        System.out.println("2ª pessoa: ");
+        maria.setsexo(entrada.nextLine());
+
+        System.out.println("Peso: ");
+        System.out.println("1ª pessoa: ");
+        joao.setpeso(entrada.nextInt());
+        System.out.println("2ª pessoa: ");
+        maria.setpeso(entrada.nextInt());
+
+        System.out.println("Altura: ");
+        System.out.println("1ª pessoa: ");
+        joao.setaltura(entrada.nextInt());
+        System.out.println("2ª pessoa: ");
+        maria.setaltura(entrada.nextInt());
+
+    }
+
 }
---
+++ 4º Período/POO I/Exercício 3 (pessoa 2.0)/Pessoa.java
@@ -0,0 +1,70 @@
+/*
+Crie uma classe para representar uma pessoa, com os atributos privados 
+de nome, sexo, peso e altura. Crie os métodos públicos necessários para 
+sets e gets e também um método para imprimir todos dados de uma pessoa. 
+Crie um método para calcular o imc da pessoa e outro método que retorne 
+string se está abaixo, acima ou no peso ideal. Faça o teste com duas 
+pessoas passando os valores por get e set.
+*/
+
+public class Pessoa {
+
+    private String nome;
+    private String sexo;
+    private int peso;
+    private int altura;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setsexo(String string){
+        this.sexo = string;
+    }
+    public void setpeso(int peso){
+        this.peso = peso;
+    }
+
+    public void setaltura(int altura){
+        this.altura = altura;
+    }
+ 
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getsexo(){
+        return sexo;
+    }
+
+    public int getpeso(){
+        return peso;
+    }
+
+    public int getaltura(){
+        return altura;
+    }
+
+    public String imprimir(){
+        return ("\nNome: "+getNome()+
+        "\nSexo: "+getsexo()+
+        "\nPeso: "+getpeso()+
+        "\nAltura: "+getaltura());
+    }
+    
+    public double calcularIMC(){
+
+        double imc = this.peso/(Math.pow(this.altura, 2));
+        return imc;
+    }
+
+    public String checarIMC(){
+
+        if(calcularIMC()<=18.5){
+            return "Abaixo do Peso.";
+        }else if (calcularIMC()>=18.5 && calcularIMC()<=24.9){
+            return "Peso Normal.";
+        }else {
+            return "Acima do Peso.";
+        }
+    }
+}
