7219222c5f34624a191b500dc01aff5eb2c83937
Davi Perdigão <davivcperdigao@gmail.com>
2021-09-03 21:23:59 -0300

    Add

---
+++ 4º Período/POO I/Carro/Carro.java
@@ -0,0 +1,17 @@
+/*Criar um projeto de  carro (novamente), onde, quando acelerar 
+o carro, caso ele entre na reserva (10% ou menos do tanque), 
+avisar o usuário. Criar o campo velocidade máxima, e o carro 
+deverá respeitar esse valor.  Veja que carros que tem motor mais 
+potente gastam mais gasolina. Lembre-se que o carro só anda ligado 
+e com gasolina, que ele precisa abastecer, etc. Você é livre para 
+escolher os atributos do carro, mas alguns são obrigatórios, como
+apresentado anteriormente. Ao criar o carro é preciso informar no 
+construtor o modelo, capacidade do tanque, velocidade máxima e potencia 
+do motor(1.0, 1.4, 1.8, etc). No método main, receba as informações
+iniciais do carro e crie um menu para o usuário interagir com o carro como...
+acelerar, frear, ligar, abastecer, etc....
+*/
+
+public class Carro {
+
+}
