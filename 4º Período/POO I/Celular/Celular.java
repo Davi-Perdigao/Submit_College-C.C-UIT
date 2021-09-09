96e63ce76f7e562d55448751bcc6b5518e7a7947
Davi Perdigão <davivcperdigao@gmail.com>
2021-09-08 21:13:43 -0300

    Commit Celular

--- 4º Período/POO I/Celular/Celular.java
+++ 4º Período/POO I/Celular/Celular.java
@@ -1,3 +1,15 @@
+/*Criar uma classe celular com os seguintes
+campos: modelo, nivelBateria, ligado (tipo boolean) 
+e som. O usuário pode carregar o celular, aumentar 
+e diminuir o som e jogar, ao jogar ele poderá escolher 
+o jogo e a bateria deverá gastar quando jogar. O jogo 
+deverá ser um par ou impar onde o usuário escolher 
+(par ou impar) e o sistema gera um número aleatorio e diz
+se ganhou ou perdeu. O usuário somente poderá controlar o 
+som e jogar se o celular estiver ligado. Criar uma classe 
+TestaCelular e criar  um menu para o usuário interagir.
+*/
+
 package Celular;
 
 public class Celular {
