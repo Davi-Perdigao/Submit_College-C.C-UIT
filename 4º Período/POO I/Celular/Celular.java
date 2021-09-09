b4e6db9f423e52b5a1f392316011b6a830277da2
Davi Perdigão <davivcperdigao@gmail.com>
2021-09-09 19:37:58 -0300

    Celular

--- 4º Período/POO I/Celular/Celular.java
+++ 4º Período/POO I/Celular/Celular.java
@@ -12,6 +12,129 @@ TestaCelular e criar  um menu para o usuário interagir.
 
 package Celular;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Celular {
+    private String modelo;
+    private Double nivelBateria;
+    private Boolean ligado;
+    private Double nivelSom;
+  
+    private final Double gastoBateria = 10d;
+    private Scanner s = new Scanner(System.in);
+  
+  
+    private void setModelo(String modelo) {
+      this.modelo = modelo;
+    }
+  
+    public String getModelo() {
+      return modelo;
+    }
+  
+    public void ligar() {
+      if (nivelBateria <= 0) {
+        System.out.println("Sem bateria!");
+        return;
+      }
+      ligado = true;
+    }
     
-}
+    public void desligar() {
+      ligado = false;
+    }
+  
+    public boolean isLigado() {
+      return ligado;
+    }
+  
+    private void usarBateria() {
+      nivelBateria -= gastoBateria;
+      checarBateria();
+    }
+  
+    private void checarBateria() {
+      if (nivelBateria <= 0) {
+        ligado = false;
+        nivelSom = 0d;
+        nivelBateria = 0d;
+      }
+    }
+  
+    public void carregarBateria() {
+      nivelBateria += gastoBateria;
+    }
+  
+    public void carregarCompletamente() {
+      while (nivelBateria < 100) {
+        carregarBateria();
+      }
+    }
+  
+    public void aumentarVolume() {
+      if (!isLigado() || nivelSom >= 100) {
+        System.out.println("Sem bateria ou volume máximo");
+        return;
+      }
+      nivelSom += 5;
+    }
+  
+    public void diminuirVolume() {
+      if (!isLigado() || nivelSom <= 0) {
+        System.out.println("Sem bateria ou volume zerado");
+        return;
+      }
+  
+      nivelSom -= 5;
+    }
+  
+    public void jogar() {
+      if (!isLigado() || nivelBateria < gastoBateria) {
+        System.out.println("Sem bateria suficiente ou desligado!");
+        return;
+      }
+      short choice = 2;
+  
+      while (choice > 1 || choice < 0) {
+        System.out.println("\n\tJOGO \nEscolha [0 - Impar | 1 - Par]: ");
+        choice = s.nextShort();
+      }
+  
+      int num = getRandomNumber();
+   
+      System.out.println( 
+        (isPar(num) && choice == 1) || (!isPar(num) && choice == 0) 
+        ? "\n\tNumero: " + num + "\nParabéns você venceu! :D\n\n" 
+        : "\n\tNumero: " + num + "\nNão foi dessa vez :(\n\n"
+      );
+  
+      usarBateria();  
+    }
+  
+    private int getRandomNumber() {
+      Random r = new Random();
+      return r.nextInt(100);
+    }
+  
+    private boolean isPar(int num) {
+      return num%2 == 0;
+    }
+  
+    public void printCelular() {
+      System.out.println("\n\tFicha Técnica"
+        + "\nModelo: " + modelo
+        + "\nNível da bateria: " + nivelBateria + "%"
+        + "\nNivel do som: " + nivelSom + "%"
+        + "\nCelular " + (ligado ? "ligado" : "desligado")
+        + "\n"
+      );
+    }
+  
+    public Celular(String modelo) {
+      nivelBateria = 0d;
+      nivelSom = 100d;
+      desligar();
+      setModelo(modelo);
+    }  
+  }
--- 4º Período/POO I/Celular/Main.java
+++ 4º Período/POO I/Celular/Main.java
@@ -1,5 +1,67 @@
 package Celular;
+import java.util.Scanner;
 
 public class Main {
+  static Scanner s = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    System.out.println("Modelo do Celular: ");
+    String modelo = s.nextLine();
+
+    Celular celular = new Celular(modelo);
     
+    menu(celular);
+  }
+
+  public static void menu(Celular celular) {
+    int resposta = 1;
+
+    do {
+      celular.printCelular();
+
+      System.out.println("\tMenu"
+        + "\n1 - Ligar celular" 
+        + "\n2 - Desligar celular" 
+        + "\n3 - Carregar" 
+        + "\n4 - Carregar completamente" 
+        + "\n5 - Aumentar volume" 
+        + "\n6 - Abaixar volume" 
+        + "\n7 - Jogar" 
+        + "\n0 - Sair do Menu" 
+        + "\n Resposta: "  
+      );
+      
+      resposta = s.nextInt();
+      
+      switch (resposta) {
+        case 1: 
+          celular.ligar();
+          break;
+        case 2:
+          celular.desligar();
+          break;
+        case 3:
+          celular.carregarBateria();
+          break;
+        case 4:
+          celular.carregarCompletamente();
+          break;
+        case 5:
+          celular.aumentarVolume();
+          break;
+        case 6:
+          celular.diminuirVolume();
+          break;
+        case 7:
+          celular.jogar();
+          break;
+        case 0:
+          System.out.println("Saindo...");
+          break;
+        default:
+          System.out.println("Opção errada");
+          break;
+      }
+    } while (resposta != 0);
+  }
 }
