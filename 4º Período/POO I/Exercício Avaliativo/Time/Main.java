d3276c5fd9bfaa72a44606363459c13b8c522deb
Davi Perdigão <davivcperdigao@gmail.com>
2021-09-17 15:05:46 -0300

    Time

--- 4º Período/POO I/Exercício Avaliativo/Time/Main.java
+++ 4º Período/POO I/Exercício Avaliativo/Time/Main.java
@@ -1,5 +1,35 @@
-package Exercício Avaliativo.Time;
+import java.util.Scanner;
 
 public class Main {
-    
+    public static void main(String[] args)
+    {
+        Scanner entrada = new Scanner(System.in);
+        Time time[] = new Time[2];
+
+        String nome, serie;
+        int numeroJogadores, i = 0;
+
+        for(i = 0; i<time.length; i++){
+            System.out.printf("%dº TIME \n", i+1);
+
+            System.out.print("Informe o nome: ");
+            nome = entrada.nextLine();
+
+            System.out.print("Informe a serie (A a E): ");
+            serie = entrada.nextLine();
+
+            time[i] = new Time(nome, serie);
+
+            System.out.print("Informe o número de jogadores: ");
+            numeroJogadores = entrada.nextInt();
+            entrada.nextLine();
+
+            time[i].setNumeroJogadores(numeroJogadores);
+        }
+
+        for(i = 0; i<time.length; i++){
+            System.out.printf("\n%dº TIME\n", i+1);
+            System.out.println(time[i]);
+        }
+    }
 }
--- 4º Período/POO I/Exercício Avaliativo/Time/Time.java
+++ 4º Período/POO I/Exercício Avaliativo/Time/Time.java
@@ -1,5 +1,74 @@
-package Exercício Avaliativo.Time;
+/*Crie a classe Time que possua o nome do time, a série 
+ue ele joga e o número de jogadores. As séries podem ser 
+de A, B, C, D ouE. O números de jogadores é no máximo 22. 
+Crie um construtor onde seja obrigatório inserir no momento 
+da criação da instância o nome e a série do time. Faça o 
+teste em outra classe cadastrando 2 times.
+*/
 
-public class Time {
+public class Time
+{
+    private String nome;
+    private String serie;
+    private int numeroJogadores;
+
+    public Time(String nome, String serie){
+        setNome(nome);
+        setSerie(serie);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    private void setSerie(String serie) {
+        char s = serie.charAt(0);
+        s = Character.toUpperCase(s);
+        
+        switch(s){
+            case 'A':
+                this.serie = "A";
+                break;
+            case 'B':
+                this.serie = "B";
+                break;
+            case 'C':
+                this.serie = "C";
+                break;
+            case 'D':
+                this.serie = "D";
+                break;
+            case 'E':
+                this.serie = "E";
+                break;
+            default:
+                this.serie = "Não informada.";
+                break;
+        } 
+    }
+
+    public int getNumeroJogadores() {
+        return numeroJogadores;
+    }
+
+    public void setNumeroJogadores(int numeroJogadores) {
+        if(numeroJogadores>0 && numeroJogadores<=22){
+            this.numeroJogadores = numeroJogadores;
+        } else{
+            this.numeroJogadores = 22;
+        }
+    }
     
+    public String toString(){
+        String mensagem = String.format("NOME: %s\nSÉRIE: %s\nJOGADORES: %d", this.nome, this.serie, this.numeroJogadores);
+        return mensagem;
+    }
 }
