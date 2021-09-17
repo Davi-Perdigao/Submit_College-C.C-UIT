78fb9286f3f1b88ac44c5417f8f86dd3d0c07625
Davi Perdigão <davivcperdigao@gmail.com>
2021-09-17 14:54:02 -0300

    Trabalhador

--- 4º Período/POO I/Exercício Avaliativo/Trabalhador/Main.java
+++ 4º Período/POO I/Exercício Avaliativo/Trabalhador/Main.java
@@ -1,5 +1,28 @@
-package Exercício Avaliativo.Trabalhador;
+import java.util.Scanner;
 
 public class Main {
-    
+    public static void main(String args[]){
+        Scanner entrada = new Scanner(System.in);
+        Trabalhador trabalhador = new Trabalhador();
+
+        System.out.println("TRABALHADORES: ");
+        for(int i=0; i<3; i++){
+            System.out.println("Matrícula: ");
+            trabalhador.setMatricula(entrada.next());
+            clearBuffer(entrada);
+        
+            System.out.println("Nome: ");
+            trabalhador.setNome(entrada.next());
+
+            System.out.println("Salário: ");
+            trabalhador.setSalario(entrada.nextDouble());
+
+        }
+    }
+
+    private static void clearBuffer(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
+    }
 }
--- 4º Período/POO I/Exercício Avaliativo/Trabalhador/Trabalhador.java
+++ 4º Período/POO I/Exercício Avaliativo/Trabalhador/Trabalhador.java
@@ -1,5 +1,56 @@
-package Exercício Avaliativo.Trabalhador;
+/*
+ * Crie a classe Trabalhador com os atributos matrícula, nome e salário. Crie uma outra classe
+ * com o método main e peça ao usuário para cadastrar 3 trabalhadores. Lembre-se que ninguém
+ * pode ganhar menos que um salário mínimo. 
+ */
 
-public class Trabalhador {
-    
+public class Trabalhador
+{
+    private String matricula;
+    private String nome;
+    private double salario;
+
+    private final Double salarioMinimo = 1147d;
+
+    public void setMatricula(String matricula)
+    {
+        this.matricula = matricula;
+    }
+
+    public String getMatricula()
+    {
+        return this.matricula;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getNome()
+    {
+        return this.nome;
+    }
+
+    public void setSalario(Double salario) {
+        if(salario >= salarioMinimo) {
+          this.salario = salario;
+        } else {
+          System.out.println("Trabalhador não deve receber menos que um salário mínimo.");
+        }
+      }
+
+    public double getSalario()
+    {
+        return this.salario;
+    }
+
+    public Trabalhador(String matricula, String nome, Double salario) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.salario = salario;
+      }
+
+    public Trabalhador() {
+    }
 }
