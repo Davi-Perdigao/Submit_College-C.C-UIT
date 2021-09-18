3b33dfd0028b87187eea91115e390d21811a1be4
Davi Perdigão <davivcperdigao@gmail.com>
2021-09-17 23:22:35 -0300

    Ex A

--- 4º Período/POO I/Prova I/A.java
+++ 4º Período/POO I/Prova I/A.java
@@ -6,15 +6,3 @@ o método retornar true então eledeve, também, alterar o valor do
 atributo a para 10 vezes o valor proposto no parâmetro do método m.
 */
 
-public class A {
-    int a;
-    public boolean m(int valor) {
-    // ? (resolução)
-        if (valor > 0){
-            a = valor * 10;
-            return true;
-        }
-        return false;
-    }
-}
-    
