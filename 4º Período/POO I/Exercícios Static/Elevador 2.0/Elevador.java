0e74e8fec4094506a3408bd73d456eaaed5f1117
Davi Perdigão <davivcperdigao@gmail.com>
2021-09-24 22:26:30 -0300

    New commit

--- 4º Período/POO I/Exercícios Static/Elevador 2.0/Elevador.java
+++ 4º Período/POO I/Exercícios Static/Elevador 2.0/Elevador.java
@@ -1,4 +1,110 @@
+/*Refazer o exercício do elevador da prova para ele 
+não poder subir ou descer sem pessoas dentro do elevador. 
+Modifique as opções para que possa escolher quantos andares 
+subir ou descer e quantas pessoas irão entrar ou sair 
+através de sobrecarga.
+*/
 
-public class Elevador {
+public class Elevador
+{
+    private int andarAtual;
+    private int totalAndares;
+    private int capacidadeElevador;
+    private int pessoasNoElevador;
+
+    public Elevador(int capacidadeElevador, int totalAndares){
+        setCapacidadeElevador(capacidadeElevador);
+        setTotalAndares(totalAndares);
+        this.andarAtual = 0;
+        this.pessoasNoElevador = 0;
+    }
+
+    public void Entrar(){
+        if(this.pessoasNoElevador<this.capacidadeElevador){
+            this.pessoasNoElevador++;
+        }
+    }
+
+    public void Entrar(int quantidadePessoas){
+        if(this.pessoasNoElevador<this.capacidadeElevador){
+            if(this.pessoasNoElevador+quantidadePessoas <= this.capacidadeElevador){
+                this.pessoasNoElevador += quantidadePessoas;
+            }
+        }
+    }
     
+    public void Sair(){
+        if(this.pessoasNoElevador>0){
+            this.pessoasNoElevador--;
+        }
+    }
+
+    public void Sair(int quantidadePessoas){
+        if(this.pessoasNoElevador>0){
+            if(this.pessoasNoElevador-quantidadePessoas >= 0){
+                this.pessoasNoElevador -= quantidadePessoas;
+            } else{
+                this.pessoasNoElevador = 0;
+            }
+        }
+    }
+
+    public void Subir(){
+        if(this.andarAtual<this.capacidadeElevador){
+            this.andarAtual++;
+        }
+    }
+
+    public void Subir(int andar){
+        if(andar<=this.capacidadeElevador && this.andarAtual<andar){
+            this.andarAtual = andar; 
+        }
+    }
+
+
+    public void Descer(){
+        if(this.andarAtual!=0){
+            this.andarAtual--;
+        }
+    }
+
+    public void Descer(int andar){
+        if(this.andarAtual!=0 && andar<=this.capacidadeElevador && this.andarAtual>andar){
+                this.andarAtual = andar;
+        }
+    }
+
+    public String Infos() {
+        String mensagem = String.format("Pessoas no elevador: %d\nAndar atual: %d\nTotal de andares: %d\nCapacidade: %d\n", this.pessoasNoElevador, this.andarAtual, this.totalAndares, this.capacidadeElevador);
+        return mensagem;
+    }
+
+    public int getAndarAtual() {
+        return andarAtual;
+    }
+
+    public int getTotalAndares() {
+        return totalAndares;
+    }
+
+    private void setTotalAndares(int totalAndares) {
+        if(totalAndares>0){
+            this.totalAndares = totalAndares;
+        }  
+    }
+
+    public int getCapacidadeElevador() {
+        return capacidadeElevador;
+    }
+
+    private void setCapacidadeElevador(int capacidadeElevador) {
+        if(capacidadeElevador>0){
+            this.capacidadeElevador = capacidadeElevador;
+        }
+    }
+
+    public int getPessoasNoElevador() {
+        return pessoasNoElevador;
+    }
+
 }
--- 4º Período/POO I/Exercícios Static/Elevador 2.0/Main.java
+++ 4º Período/POO I/Exercícios Static/Elevador 2.0/Main.java
@@ -1,5 +1,66 @@
-package Exercícios Static.Elevador 2.0;
+import java.util.Scanner;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        int capacidadeMaxima, opcao, quantidadePessoas, andar, resposta;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Capacidade do Elevador:");
+        capacidadeMaxima = input.nextInt();
+        System.out.println("Total Andares: ");
+        int totalAndares = input.nextInt();
+
+        Elevador elevador = new Elevador(capacidadeMaxima, totalAndares);
+
+        do {
+            menu();
+            opcao = input.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Informe a quantidade de pessoas que vão entrar:");
+                    resposta = input.nextInt();
+                    if (resposta<=capacidadeMaxima){
+                    quantidadePessoas = input.nextInt();
+                    elevador.Entrar(quantidadePessoas);
+                    } else{
+                        resposta=capacidadeMaxima;
+                    }
+                    break;
+                case 2:
+                    System.out.println("Informe a quantidade de pessoas que vão sair:");
+                    quantidadePessoas = input.nextInt();
+                    elevador.Sair(quantidadePessoas);
+                    break;
+                case 3:
+                    System.out.println("Informe a quantidade de andares que deseja subir:");
+                    andar = input.nextInt();
+                    elevador.Subir(andar);
+                    break;
+                case 4:
+                    System.out.println("Informe a quantidade de andares que deseja descer:");
+                    andar = input.nextInt();
+                    elevador.Descer(andar);
+                break;
+                case 5:
+                    System.out.println(elevador.Infos());
+                    break;
+                case 6:
+                    System.out.println("Finalizando...");
+                    break;
+                default:
+                    System.out.println("Escolha uma opção válida!");
+                    break;
+            }
+        } while (opcao != 6);
+    }
+
+    public static void menu() {
+        System.out.println("Informe a opção: ");
+        System.out.println("1 - Entrar");
+        System.out.println("2 - Sair");
+        System.out.println("3 - Subir");
+        System.out.println("4 - Descer");
+        System.out.println("5 - Exibir informações");
+        System.out.println("6 - Finalizar tarefa");
+    }
 }
