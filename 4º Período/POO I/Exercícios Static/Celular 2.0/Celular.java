1fac05e37ab5a5bde95b2ecc3826ad6bd0cb15fd
Davi Perdigão <davivcperdigao@gmail.com>
2021-09-25 23:39:04 -0300

    Celular OK

--- 4º Período/POO I/Exercícios Static/Celular 2.0/Celular.java
+++ 4º Período/POO I/Exercícios Static/Celular 2.0/Celular.java
@@ -8,118 +8,138 @@ que retorna quantos celulares estão instanciados!
 import java.util.Random;
 import java.util.Scanner;
 
-public class Celular
-{
-  private String modelo;
-  private int nivelBateria;
-  private boolean ligado;
-  private int som;
+public class Celular {
+    private String modelo;
+    private int nivelBateria;
+    private boolean ligado;
+    private int som;
+    private static int contInstancias = 1;
+    private static int idCelular;
 
-  public Celular(String modelo, int nivelBateria, int nivelSom){
-      setModelo(modelo);
-      setNivelBateria(nivelBateria);
-      setSom(nivelSom); 
-  }
+    Scanner input = new Scanner(System.in);
 
-  private void setModelo(String modelo){
-      this.modelo = modelo;
-  }
+    public Celular(String modelo, int nivelBateria, int nivelSom) {
+        setModelo(modelo);
+        setNivelBateria(nivelBateria);
+        setSom(nivelSom);
+        idCelular = contInstancias;
+        contInstancias++;
+    }
 
-  public String getModelo(){
-      return this.modelo;
-  }
-  public int getNivelBateria() {
-      return nivelBateria;
-  }
+    public Celular(String modelo, int nivelBateria){
+    }
 
-  private void setNivelBateria(int nivelBateria) {
-      if(nivelBateria>0 && nivelBateria<=100){
-          this.nivelBateria = nivelBateria;
-      }
-  }
+    private void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 
-  public void carregarBateria(){
-      if(this.nivelBateria<100){
-          this.nivelBateria++;
-      }
-  }
+    public String getModelo() {
+        return this.modelo;
+    }
 
-  public boolean isLigado(){
-      return this.ligado;
-  }
+    public int getNivelBateria() {
+        return nivelBateria;
+    }
 
-  public String ligar() {
-      if(this.nivelBateria > 0){
-          this.ligado = true;
-          return "Celular ligado.";
-      } else{
-          return "Sem bateria!";
-      }
-      
-  }
-  
-  private void setSom(int som){
-      this.som = som;
-  }
+    private void setNivelBateria(int nivelBateria) {
+        if (nivelBateria > 0 && nivelBateria <= 100) {
+            this.nivelBateria = nivelBateria;
+        }
+    }
 
-  public int getSom() {
-      return som;
-  }
+    public void carregarBateria() {
+        if (this.nivelBateria < 100) {
+            this.nivelBateria++;
+        }
+    }
 
-  /*O usuário somente poderá controlar o 
-  som e jogar se o celular estiver ligado.
-  */
-  
-  public String aumentarVolume(int som) {
-      if(this.nivelBateria>0 && this.ligado == true){
-          if(som>0 && som<=100){
-              if((this.som+som)>100){
-                  this.som = 100;
-              } else{
-                  this.som += som;
-              } 
-          } else{
-              return "Valor inválido!";
-          }
-          return "Nivel de volume: " + getSom();
-      } else{
-          return "Volume máximo!";
-      }
-  }
+    public boolean isLigado() {
+        return this.ligado;
+    }
 
-  public String diminuirVolume(int som){
-      if(this.nivelBateria>0 && this.ligado == true){
-          if(som>0 && som<=100){
-              if((this.som-som)<0){
-                  this.som = 0;
-              } else{
-                  this.som -= som;
-              } 
-          } else{
-              return "Valor inválido!";
-          }
-          return "Nivel de volume: " + getSom();
-      } else{
-          return "O volume está no mínimo!.";
-      } 
-  }
+    public String ligar() {
+        if (this.nivelBateria > 0) {
+            this.ligado = true;
+            return "Celular ligado.";
+        } else {
+            return "Sem bateria!";
+        }
 
-  /*O jogo deverá ser um par ou impar onde o usuário escolher 
-  (par ou impar) e o sistema gera um número aleatorio e diz
-  se ganhou ou perdeu.
-  */
+    }
 
-  public String jogar(int num){
-      if(this.nivelBateria>0 && this.ligado == true){
-          Random numeroRandom = new Random();
-          this.nivelBateria--; //a bateria deverá gastar quando jogar
-          if(numeroRandom.nextInt(5) == num){
-              return "Você venceu :D !!!";
-          } else{
-              return "Ooops! Você perdeu...";
-          }
-      } else{
-          return "Não é possível jogar. Verifique o nível da bateria, ou se o telefone está ligado.";
-      }   
-  }
+    private void setSom(int som) {
+        this.som = som;
+    }
+
+    public int getIdCelular(){
+        return this.idCelular;
+    }
+
+    public int getSom() {
+        return som;
+    }
+
+    /*
+     * O usuário somente poderá controlar o som e jogar se o celular estiver ligado.
+     */
+
+    public String aumentarVolume(int som) {
+        if (this.nivelBateria > 0 && this.ligado == true) {
+            if (som > 0 && som <= 100) {
+                if ((this.som + som) > 100) {
+                    this.som = 100;
+                } else {
+                    this.som += som;
+                }
+            } else {
+                return "Valor inválido!";
+            }
+            return "Nivel de volume: " + getSom();
+        } else {
+            return "Volume máximo!";
+        }
+    }
+
+    public String diminuirVolume(int som) {
+        if (this.nivelBateria > 0 && this.ligado == true) {
+            if (som > 0 && som <= 100) {
+                if ((this.som - som) < 0) {
+                    this.som = 0;
+                } else {
+                    this.som -= som;
+                }
+            } else {
+                return "Valor inválido!";
+            }
+            return "Nivel de volume: " + getSom();
+        } else {
+            return "O volume está no mínimo!.";
+        }
+    }
+
+    //altere o jogo para  poder jogar Pedra, Papel e Tesoura
+
+    public int jogar(int escolhaJogador){
+        Random numeroRandom = new Random();
+        int escolhaPC = numeroRandom.nextInt(3);
+        this.nivelBateria--;
+        if(escolhaPC!=escolhaJogador){
+            if((escolhaJogador==1 && escolhaPC==1) || (escolhaJogador==2 && escolhaPC==2) || (escolhaJogador==3 && escolhaPC==0)){
+                return 0; //vitória
+            } else{
+                return 1; //derrota
+            }
+        } else{
+            return 2; //empate
+        }
+}
+
+    public String qtdCelular() {
+        return String.format("Quantidade de Celulares: %d", contInstancias-1);
+    }
+
+    public String toString(){
+        String mensagem = String.format("Modelo: %s\nID: %d\nNivel da batéria: %d%%\nVolume atual: %d\n", this.modelo, this.idCelular, this.nivelBateria, this.som);
+        return mensagem;
+    }
 }
--- 4º Período/POO I/Exercícios Static/Celular 2.0/Main.java
+++ 4º Período/POO I/Exercícios Static/Celular 2.0/Main.java
@@ -1,77 +1,90 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
+    public static void main(String[] args){
 
-  public static void main(String[] args){
+        int nivelBateria, nivelSom, opcao, som;
+        String modelo;
 
-    int nivelBateria, nivelSom, opcao, som;
-    String modelo;
-
-    Scanner entrada = new Scanner(System.in);
-
-    System.out.println("Informe o modelo do celular:");
-    modelo = entrada.nextLine();
-    System.out.println("Informe o nível da bateria:");
-    nivelBateria = entrada.nextInt();
-    System.out.println("Informe o nível do som:");
-    nivelSom= entrada.nextInt();
-
-    Celular modelX = new Celular(modelo, nivelBateria, nivelSom);
-
-    do{
-        System.out.println("Menu:"); /*O usuário pode carregar o celular, aumentar 
-                                                      e diminuir o som e jogar.
-                                                    */  
-        System.out.println("1 - Ligar");
-        System.out.println("2 - Carregar");
-        System.out.println("3 - Jogar");
-        System.out.println("4 - Aumentar som");
-        System.out.println("5 - Diminuir som");
-        System.out.println("0 - Sair");
+        Scanner entrada = new Scanner(System.in);
         
-        opcao = entrada.nextInt();
+        System.out.println("Informe o modelo do celular: ");
+        modelo = entrada.nextLine();
+        System.out.println("Informe o nível da bateria: ");
+        nivelBateria = entrada.nextInt();
+        System.out.println("Informe o nível do som: ");
+        nivelSom= entrada.nextInt();
+        
+        Celular celular1 = new Celular(modelo, nivelBateria, nivelSom);
+        
+        do{
+            menu();
+            opcao = entrada.nextInt();
 
-        switch(opcao){
-            case 1:
-                System.out.println(modelX.ligar());
-                break;
-            case 2:
-                modelX.carregarBateria();
-                System.out.println("A bateria está em " + modelX.getNivelBateria() + "%");
-                break;
-            case 3:
-                if(modelX.getNivelBateria()>0 && modelX.isLigado()){
-                    int numero;
-                    System.out.println("O jogo é um Par OU Impar");   
-                    System.out.print("Escolha um número: ");
-                    numero = entrada.nextInt();
+            switch(opcao){
+                case 1:
+                    celular1.ligar();
+                    break;
+                case 2:
+                    celular1.carregarBateria();
+                    System.out.println("Bateria: " + celular1.getNivelBateria() + "%");
+                    break;
+                case 3:
+                    if(celular1.getNivelBateria()>0 && celular1.isLigado()){
+                        int valor, resultado;
+                        do{
+                            System.out.println("Jogo inicializado: PEDRA, PAPEL OU TESOURA");   
+                            System.out.println("Qual será sua escolha? ");
+                            System.out.println("0 - Pedra\n1 - Papel\n2 - Tesoura");
 
-                    System.out.println(modelX.jogar(numero));
-                    System.out.println(modelX.getNivelBateria());
-                } else{
-                    System.out.println("Não foi possível jogar.");
-                }
-                break;
-            case 4:
-                
-                System.out.println("Informe o nível de volume que deseja aumentar:");
-                som = entrada.nextInt();
-                System.out.println(modelX.aumentarVolume(som));
-                break;
-            case 5:
-                System.out.println("Informe o nível de volume que deseja diminuir:");
-                som = entrada.nextInt();
-                System.out.println(modelX.diminuirVolume(som));
+                            valor= entrada.nextInt();
+                        }while(valor<0 || valor>2);
+    
+                        resultado = celular1.jogar(valor);
 
-            case 0:
-                System.out.println("Finalizando Menu...");
-                break;
+                        if(resultado==0){
+                            System.out.println("Você perdeu :(");
+                        } else if(resultado==1){
+                            System.out.println("Você ganhou :)");
+                        } else{
+                            System.out.println("Empate :|");
+                        }
+                    } else{
+                        System.out.println("Não foi possível jogar.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Informe o nível de volume que deseja aumentar: ");
+                    som = entrada.nextInt();
+                    celular1.aumentarVolume(som);
+                    break;
+                case 5:
+                    System.out.println("Informe o nível de volume que deseja diminuir: ");
+                    som = entrada.nextInt();
+                    celular1.diminuirVolume(som);
+                    break;
+                case 6:
+                    System.out.println(celular1);
+                    break;
+                case 7:
+                    System.out.println("Finalizando...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        } while(opcao!=7);
+        entrada.close();
+    }
 
-            default:
-                System.out.println("Opção inválida!");
-                break;
-        }
-    } while (opcao != 0);
-    entrada.close();
-}
+    public static void menu(){
+        System.out.println("MENU:");
+        System.out.println("1 - Ligar");
+        System.out.println("2 - Recarregar");
+        System.out.println("3 - Jogar");
+        System.out.println("4 - Aumentar volume");
+        System.out.println("5 - Diminuir volume");
+        System.out.println("6 - Mostrar informações");
+        System.out.println("7 - Sair");
+    }
 }
