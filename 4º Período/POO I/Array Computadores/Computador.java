62bf4b7bb204f8fbb193b4767f9aa3a4462ee796
Davi Perdigão <davivcperdigao@gmail.com>
2021-10-04 20:36:01 -0300

    Computador OK

--- 4º Período/POO I/Array Computadores/Computador.java
+++ 4º Período/POO I/Array Computadores/Computador.java
@@ -1,5 +1,96 @@
-package Array Computadores;
+/*
+ Crie uma classe Computador que possua os parâmetros tamanho da HD, tamanho da
+ RAM, número de processadores e fabricante. Crie métodos de acesso público e
+ reimplemente o método toString() da classe. Crie um construtor para passar
+ todos os atributos durante a criação do objeto.
+
+ Crie o atributo booleano ligado e um método para ligar e desligar o
+ computador. Todo computador quando for criado deve estar desligado. Crie o
+ método estadoComputador que retorna se ele está ligado ou desligado. 
+
+ Crie uma classe TestaComputador. Crie nesta classe um array de 3 computadores
+ que irá receber os dados do computador pelo usuário. Depois imprima os dados
+ dos computadores que possuem mais que 512Mb de Ram usando o método toString.
+ 
+  Se conseguir fazer o cadastro por menu é muito bom. 
+ */
 
 public class Computador {
-    
+
+    private int tamanhoHD;
+    private int tamanhoRAM;
+    private int numProcessadores;
+    private String fabricante;
+    public static int RAMaceita = 512;
+    private boolean ligado;
+
+    public Computador(int tamanhoHD, int tamanhoRAM, int numProcessadores, String Fabricante) {
+        this.tamanhoHD = tamanhoHD;
+        this.tamanhoRAM = tamanhoRAM;
+        this.numProcessadores= numProcessadores;
+        this.fabricante = Fabricante;
+        this.ligado = false;
+    }
+
+    public void ligarComputador() {
+        ligado = true;
+    }
+
+    public void desligarComputador() {
+        ligado = false;
+    }
+
+    public double getTamanhoHD() {
+        return tamanhoHD;
+    }
+
+    public void setTamanhoHD(double tamanhoHD) {
+        if (tamanhoHD <= 0)
+            return;
+
+        this.tamanhoHD = tamanhoHD;
+    }
+
+    public double getTamanhoRAM() {
+        return tamanhoRAM;
+    }
+
+    public void setTamanhoRAM(double tamanhoRAM) {
+        if (tamanhoRAM <= 0)
+            return;
+
+        this.tamanhoRAM = tamanhoRAM;
+    }
+
+    public int getNumProcessadores() {
+        return numProcessadores;
+    }
+
+    public void setNumProcessadores(int numProcessadores) {
+        if (numProcessadores <= 0)
+            return;
+
+        this.numProcessadores = numProcessadores;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String Fabricante) {
+        this.fabricante = Fabricante;
+    }
+
+    public String toString() {
+        return "\nComputador: "+fabricante+"\nLigado: "+ligado+"\nTamanhoHD: "+tamanhoHD+
+        "\nTamanhoRAM: "+tamanhoRAM+"\nNumero de Processadores: "+numProcessadores;
+    }
+
+    public String estadoComputador(){
+        if(ligado){
+        return "Computador ligado.";
+        }else{
+            return "Computador desligado!";
+        }
+    }
 }
--- 4º Período/POO I/Array Computadores/TestaComputador.java
+++ 4º Período/POO I/Array Computadores/TestaComputador.java
@@ -1,5 +1,71 @@
-package Array Computadores;
+import java.util.Scanner;
 
 public class TestaComputador {
-    
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        Computador[] computador = new Computador[3]; 
+
+        for (int i=0; i<3; i++) {
+            System.out.println("HD computador " + (i + 1) + " (GB): ");
+            int tamanhoHD = input.nextInt();
+            System.out.println("RAM computador " + (i + 1) + " (MB): ");
+            int tamanhoRAM = input.nextInt();
+            System.out.println("Numero de processadores computador " + (i + 1) + ": ");
+            int numProcessadores = input.nextInt();
+            input.nextLine();
+            System.out.println("Fabricante computador " + (i + 1) + ": ");
+            String fabricante = input.nextLine();
+
+            computador[i] = new Computador(tamanhoHD, tamanhoRAM, numProcessadores, fabricante);
+        }
+
+        for (int i = 0; i < computador.length; i++) {
+            if (computador[i].getTamanhoRAM() >Computador.RAMaceita) {
+                System.out.println(computador[i].toString());
+            }
+        }
+
+        int opcao = 0;
+        System.out.println("\nSe algum computador não foi mostrado é porque ele possui menos que 512Mb de RAM. Porém, agora você pode escolher o Nº de qualquer notebook para interagir:");
+        int num = input.nextInt() - 1;
+        do {
+
+            System.out.println("\n---MENU---");
+            System.out.println(("1- Escolher outro computador"));
+            System.out.println("2- Ligar");
+            System.out.println("3- Desligar");
+            System.out.println("4- Estado");
+            System.out.println("5- Informaçoes");
+            System.out.println("6- Sair");
+            opcao = input.nextInt();
+            switch (opcao) {
+                case 1:
+                    for (int i = 0; i < computador.length; i++) {
+                        System.out.println("Escolha um dos computadores: " + (i + 1) + "º" + computador[i].toString());
+                    }
+                    System.out.println("Qual deseja escolher? (Nº)");
+                    num = input.nextInt() - 1;
+                    break;
+                case 2:
+                    computador[num].ligarComputador();
+                    break;
+                case 3:
+                    computador[num].desligarComputador();
+                case 4:
+                    System.out.println(computador[num].estadoComputador());
+                    break;
+                case 5:
+                    System.out.println(computador[num].toString());
+                    break;
+                case 6:
+                    System.out.println("Finalizando...");
+                    break;
+                default:
+            }
+        } while (opcao != 0);
+        input.close();
+    }
 }
