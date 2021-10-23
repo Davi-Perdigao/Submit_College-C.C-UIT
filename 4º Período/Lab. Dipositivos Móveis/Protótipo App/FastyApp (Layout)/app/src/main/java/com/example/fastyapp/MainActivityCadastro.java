43691e5c97a987e772473041759214ce91e3f600
Davi Perdigão <davivcperdigao@gmail.com>
2021-10-23 20:21:24 -0300

    Protótipo OK

---
+++ 4º Período/Lab. Dipositivos Móveis/Protótipo App/FastyApp (Layout)/app/src/main/java/com/example/fastyapp/MainActivityCadastro.java
@@ -0,0 +1,15 @@
+package com.example.fastyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_cadastro);
+    }
+
+}
---
+++ 4º Período/Lab. Dipositivos Móveis/Protótipo App/FastyApp (Layout)/app/src/main/java/com/example/fastyapp/MainActivityReserva.java
@@ -0,0 +1,14 @@
+package com.example.fastyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityReserva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_reserva);
+    }
+}
