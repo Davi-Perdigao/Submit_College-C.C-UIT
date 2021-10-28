f926d58ed628d10a135c04054a06d355f05607b8
Davi Perdigão <davivcperdigao@gmail.com>
2021-10-28 15:00:29 -0300

    Comentários OK

--- 4º Período/POO I/Prova II/Principal.java
+++ 4º Período/POO I/Prova II/Principal.java
@@ -4,26 +4,26 @@ Preencha este vetor com números aleatórios. Após receber os números, escreva
 os mesmos de forma ordenada. 
 */
 
-import java.util.Random;
+import java.util.Random; //Objeto gerador de número aleatório
 import java.util.Arrays;
 
 public class Principal {
     
     public static void main(String[] args){
         int i;
-        Random numeroRandom = new Random();
-        int[] vet = new int[10];
+        Random numeroRandom = new Random(); //Produz estes números em resposta a solicitações
+        int[] vet = new int[10]; //array de inteiros de tamanho 10
 
-        for(i=0; i<vet.length; i++){
-            vet[i]=numeroRandom.nextInt(1000);
+        for(i=0; i<vet.length; i++){ //Preenchendo este vetor com números aleatórios
+            vet[i]=numeroRandom.nextInt(1000); 
         }
 
-        System.out.println("Numeros aleatorios:");
+        System.out.println("Numeros aleatorios:"); //recebendo números
         System.out.println(Arrays.toString(vet));
 
         Arrays.sort(vet);
 
-        System.out.println("Numeros ordenados:");
+        System.out.println("Numeros ordenados:"); //ordenando números
         System.out.println(Arrays.toString(vet));
     }
 }
