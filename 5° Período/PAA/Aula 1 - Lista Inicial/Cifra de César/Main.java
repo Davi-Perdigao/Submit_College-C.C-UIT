547ef5a903c87761dd23bbc335ead9dc498432e6
Davi Perdigão <davivcperdigao@gmail.com>
2022-03-03 14:40:28 -0300

    New Commit

--- 5° Período/PAA/Aula 1 - Lista Inicial/Cifra de César/Main.java
+++ 5° Período/PAA/Aula 1 - Lista Inicial/Cifra de César/Main.java
@@ -1,3 +1,20 @@
+/*Júlio César usava um sistema de criptografia, agora conhecido como Cifra de César, 
+que trocava cada letra pelo equivalente em duas posições adiante no alfabeto (por exemplo, 
+'A' vira 'C', 'R' vira 'T', etc.). Ao final do alfabeto nós voltamos para o começo, isto é 'Y' 
+vira 'A'. Nós podemos, é claro, tentar trocar as letras com quaisquer número de posições.
+
+Entrada
+A entrada contém vários casos de teste. A primeira linha de entrada contém um inteiro N 
+que indica a quantidade de casos de teste. Cada caso de teste é composto por duas linhas. 
+A primeira linha contém uma string com até 50 caracteres maiúsculos ('A'-'Z'), que é a sentença 
+após ela ter sido codificada através desta Cifra de César modificada. A segunda linha contém um 
+número que varia de 0 a 25 e que representa quantas posições cada letra foi deslocada para a direita.
+
+Saída
+Para cada caso de teste de entrada, imprima uma linha de saída com o texto decodificado 
+(transformado novamente para o texto original) conforme as regras acima
+*/
+
 import java.util.Scanner;
 
 public class Main{
--- 5° Período/PAA/Aula 1 - Lista Inicial/Produto Simples/Main.java
+++ 5° Período/PAA/Aula 1 - Lista Inicial/Produto Simples/Main.java
@@ -1,3 +1,8 @@
+/*Leia dois valores inteiros. A seguir, calcule o produto entre estes 
+dois valores e atribua esta operação à variável PROD. A seguir mostre a 
+variável PROD com mensagem correspondente. 
+*/  
+
 import java.util.Scanner;
 public class Main
 {
