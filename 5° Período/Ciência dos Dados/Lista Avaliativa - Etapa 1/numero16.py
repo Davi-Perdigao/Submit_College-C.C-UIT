3b40d5d6b6ce2e6c53d0ae71442d71e2d8c3fb7b
Davi Perdigão <davivcperdigao@gmail.com>
2022-03-29 00:13:29 -0300

    16

--- 5° Período/Ciência dos Dados/Lista Avaliativa - Etapa 1/numero16.py
+++ 5° Período/Ciência dos Dados/Lista Avaliativa - Etapa 1/numero16.py
@@ -2,7 +2,7 @@
 Leia um texto de um arquivo e crie um dicionário que conte o número de 
 vezes que cada letra apareceu no texto. Crie seu próprio arquivo de teste.
 '''
-texto = open('Ciência de Dados\\Lista Avaliativa - Etapa 1\\numero16\\teste16', 'r')
+texto = open("Lista Avaliativa - Etapa 1/teste16.txt", "r")
 dicionario = dict()
 for linha in texto:
     linha = linha.rstrip()
