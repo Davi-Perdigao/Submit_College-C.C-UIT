b29103dcaa04dbf88d93ff0f8c5ad46a440321b1
Davi Perdigão <davivcperdigao@gmail.com>
2023-02-22 10:29:21 -0300

    Lista0 OK

---
+++ 7º Período/Machine Learning/Lista 0 -  Revisão Programação Python/printElementVet.py
@@ -0,0 +1,3 @@
+#Vetor como entrada e print o primeiro e ultimo elemento
+times = ["CAM", "Galo", "Atlético"]
+print(times[0], ',', times[2])
---
+++ 7º Período/Machine Learning/Lista 0 -  Revisão Programação Python/workProcedure.py
@@ -0,0 +1,7 @@
+inteiros = [1, 2, 3, 4, 5]
+print("Tamanho:", len(inteiros))
+print("Menor elemento:", min(inteiros))
+print("Maior elemento:", max(inteiros))
+print("Soma dos elementos:", sum(inteiros))
+print("Média dos elementos:", sum(inteiros)/5)
+print("Penúltimo elemento:", inteiros[3])
