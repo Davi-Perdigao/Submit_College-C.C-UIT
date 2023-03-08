e3e769cc6c5908feb59f71d269e2eca23045f8ca
Davi Perdigão <68405831+Davi-Perdigao@users.noreply.github.com>
2023-03-08 16:13:18 -0300

    Delete media.py

--- 7º Período/Machine Learning/Lista 0 -  Revisão Programação Python/media.py
+++
@@ -1,9 +0,0 @@
-# Pedindo as entradas
-num1 = float( input("Digite o primeiro número: ") )
-num2 = float( input("Digite o segundo número: ") )
-num3 = float( input("Digite o terceiro número: ") )
-
-# Cálculo da média
-media = (num1 + num2 + num3) / 3
-
-print("A média desses números é: ", media)
