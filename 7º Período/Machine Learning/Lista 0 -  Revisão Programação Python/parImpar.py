614d76f531799505b0f916a734111dd8bd8080cf
Davi Perdigão <68405831+Davi-Perdigao@users.noreply.github.com>
2023-03-08 16:13:31 -0300

    Delete parImpar.py

--- 7º Período/Machine Learning/Lista 0 -  Revisão Programação Python/parImpar.py
+++
@@ -1,5 +0,0 @@
-numero = int(input('Digite um número inteiro: ')) #resto da divisão por 2, o resultado é sempre 0 ou 1.
-if numero%2 : #se o resultado da operação for 1, cai no IF que diz que é ímpar,se for 0 vai pro ELSE que diz que é par.
-    print("Número Ímpar")
-else: 
-    print("Número Par")
