87d7781260bd3fbab043eed067363b2e88a2e857
Davi Perdigão <68405831+Davi-Perdigao@users.noreply.github.com>
2023-03-08 16:13:24 -0300

    Delete palindromo.py

--- 7º Período/Machine Learning/Lista 0 -  Revisão Programação Python/palindromo.py
+++
@@ -1,6 +0,0 @@
-terminal = input("Informe uma palavra ou expressão: \n").upper().replace(' ', '')
-invertida = terminal[::-1]
-if terminal == invertida:
-    print("É palíndromo")
-else:
-    print("Não é um palíndromo")
